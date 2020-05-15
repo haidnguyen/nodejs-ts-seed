a8e831d4b66030fa237d6685bd93715f64561c95
Hai Nguyen <haidnguyen@kms-technology.com>
2020-05-16 00:09:56 +0700

    starter kit for nodejs with TS, prettier setup

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,1 @@
-import { resolve } from 'path';
-import { readFile } from 'fs';
-
-readFile(resolve(__dirname, '../assets/test.txt'), 'utf-8', (err, data) => {
-  if (err) {
-    return;
-  }
-  console.log(data);
-  return data;
-});
+export {};
